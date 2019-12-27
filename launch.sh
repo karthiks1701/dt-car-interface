40ac8097b12fe063d872b051faa8e3383cf1a547
Andrea F. Daniele <afdaniele@ttic.edu>
2019-12-27 17:49:08 -0600

    using dt_exec in launch.sh

--- launch.sh
+++ launch.sh
@@ -4,5 +4,4 @@ set -e
 
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
-roslaunch --wait car_interface all.launch veh:=$VEHICLE_NAME
-
+dt_exec roslaunch --wait car_interface all.launch veh:=$VEHICLE_NAME
