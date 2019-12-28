dfbf09e3d4da393fd6c70b35d1a7c65d6f6f8423
Andrea F. Daniele <afdaniele@ttic.edu>
2019-12-27 18:29:24 -0600

    using dt_exec in launch.sh

--- launch.sh
+++ launch.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-set -e
+source /environment.sh
+
+# initialize launch file
+dt_launchfile_init
 
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
+
+
+# NOTE: Use the variable CODE_DIR to know the absolute path to your code
+# NOTE: Use `dt_exec COMMAND` to run the main process (blocking process)
+
+# launching app
 dt_exec roslaunch --wait car_interface all.launch veh:=$VEHICLE_NAME
+
+
+# ----------------------------------------------------------------------------
+# YOUR CODE ABOVE THIS LINE
+
+# terminate launch file
+dt_launchfile_terminate
